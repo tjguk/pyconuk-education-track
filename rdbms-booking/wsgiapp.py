78698709a55e16d8ea2f54b6e7d0a5ae07befe80
Tim Golden <mail@timgolden.me.uk>
2014-09-19 17:26:51 +0100

    Noddy WSGI App to show how you might do a really simple web app

---
+++ rdbms-booking/wsgiapp.py
@@ -0,0 +1,25 @@
+from wsgiref.util import setup_testing_defaults
+from wsgiref.simple_server import make_server
+
+# A relatively simple WSGI application. It's going to print out the
+# environment dictionary after being updated by setup_testing_defaults
+def simple_app(environ, start_response):
+    setup_testing_defaults(environ)
+
+    status = '200 OK'
+    headers = [('Content-type', 'text/plain; charset=utf-8')]
+
+    start_response(status, headers)
+
+    if environ['PATH_INFO'].startswith("/users"):
+        return ["Tim is great"]
+    elif environ['PATH_INFO'].startswith("/rooms/"):
+        return ['Rooms are great']
+
+    ret = [("%s: %s\n" % (key, value)).encode("utf-8")
+           for key, value in environ.items()]
+    return ret
+
+httpd = make_server('', 8000, simple_app)
+print("Serving on port 8000...")
+httpd.serve_forever()
